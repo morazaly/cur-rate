9908558dd245ec1b29e3898d4d6f1717ec7d88a3
mashayor <--all>
2024-07-05 14:51:35 +0500

    deleted commits

--- main.go
+++ main.go
@@ -13,8 +13,6 @@ var adb *sql.DB
 var aconfig config.Config
 
 func init() {
-	//var err error
-
 	// Загрузка конфигурации из файла config.json
 	aconfig = *config.NewConfig()
 
@@ -26,14 +24,6 @@ func init() {
 func main() {
 
 	// Создание маршрута и запуск веб-сервера
-
-	/*r := mux.NewRouter()
-	r.HandleFunc("/currency/save/{date}", service.FetchFromApi(db)).Methods("GET")
-	r.HandleFunc("/currency/{date}/{code}", service.GetFromApi(db)).Methods("GET")
-	r.HandleFunc("/currency/{date}", service.GetFromApi(db)).Methods("GET")
-
-	log.Println("Server started at ", aconfig.AppPort)
-	log.Fatal(http.ListenAndServe(aconfig.AppPort, r))*/
 	r := mux.NewRouter()
 	h := http_handler.NewHandler(adb)
 	h.Save(r, aconfig)
